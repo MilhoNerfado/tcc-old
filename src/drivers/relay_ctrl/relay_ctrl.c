8a95b9fae1355fb06e2b14f09543183a969f7547
Emilio Bottoni <emilio.bottoni@edge.ufal.br>
2024-07-25 16:34:56 -0300

    chore: Add relay controller driver files

---
+++ src/drivers/relay_ctrl/relay_ctrl.c
@@ -0,0 +1,8 @@
+//
+// Created by milho on 7/23/24.
+//
+
+#include "relay_ctrl.h"
+
+
+int relay_ctrl_init();
---
+++ src/drivers/relay_ctrl/relay_ctrl.h
@@ -0,0 +1,15 @@
+//
+// Created by milho on 7/23/24.
+//
+
+#ifndef RELAY_CTRL_H
+#define RELAY_CTRL_H
+
+
+
+int relay_ctrl_init();
+
+
+
+
+#endif //RELAY_CTRL_H
