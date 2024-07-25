06d94f0ffc473aa35f8cc100b55f24d2651884f1
Emilio Bottoni <emilio.bottoni@edge.ufal.br>
2024-07-25 16:59:02 -0300

    WIP inserting relay pins

--- src/drivers/relay_ctrl/relay_ctrl.c
+++ src/drivers/relay_ctrl/relay_ctrl.c
@@ -4,5 +4,9 @@
 
 #include "relay_ctrl.h"
 
+#include <zephyr/drivers/gpio.h>
+
+static const struct gpio_dt_spec relay_enable_pin =
+	GPIO_DT_SPEC_GET_OR(DT_NODELABEL(enable), gpios, 0);
 
 int relay_ctrl_init();
