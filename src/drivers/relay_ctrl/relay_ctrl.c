62c1c93fa7ade1496f828e47b1b75edf89c93cb7
Emilio Bottoni <emilio.bottoni@edge.ufal.br>
2024-07-26 18:08:33 -0300

    feat: Implements basic relay controls

--- src/drivers/relay_ctrl/relay_ctrl.c
+++ src/drivers/relay_ctrl/relay_ctrl.c
@@ -15,9 +15,21 @@ static const struct gpio_dt_spec data_pin_spec = GPIO_DT_SPEC_GET(DT_NODELABEL(r
 // static const struct gpio_dt_spec reset_pin_spec =
 // 	GPIO_DT_SPEC_GET(DT_NODELABEL(relay), reset_gpios);
 
+static int relay_data_push(bool data);
+
+static struct self {
+	bool is_init;
+} self = {
+	.is_init = false,
+};
+
 int relay_ctrl_init(void)
 {
 	int err;
+
+	if (self.is_init) {
+		return 0;
+	}
 
 	if (gpio_is_ready_dt(&enable_pin_spec) != 0) {
 		return -ENODEV;
@@ -42,11 +54,41 @@ int relay_ctrl_init(void)
 		return err;
 	}
 
+	self.is_init = true;
+
 	return 0;
 }
 
 int relay_ctrl_set(uint32_t toggle)
 {
+	if (!self.is_init) {
+		return -ENOEXEC;
+	}
+
+	for (int i = 0; i < CONFIG_RELAY_LEN; ++i) {
+		relay_data_push(toggle & 1);
+		toggle >>= 1;
+	}
+
+	/* TODO: toggle enable. Should check data sheet for behaviour */
+	gpio_pin_set_dt(&enable_pin_spec, 0);
+	gpio_pin_set_dt(&enable_pin_spec, 1);
+	gpio_pin_set_dt(&enable_pin_spec, 0);
+
+	return 0;
+}
+
+static int relay_data_push(const bool data)
+{
+	if (!self.is_init) {
+		return -ENOEXEC;
+	}
+
+	gpio_pin_set_dt(&data_pin_spec, data);
+
+	/* TODO: Toggle clock. Should check data sheet for behaviour */
+	gpio_pin_set_dt(&clock_pin_spec, 1);
+	gpio_pin_set_dt(&clock_pin_spec, 0);
 
 	return 0;
 }
