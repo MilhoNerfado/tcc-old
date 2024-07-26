e0ffca8fbdd7b997db79d6873f159c3716e70811
Emilio Bottoni <emilio.bottoni@edge.ufal.br>
2024-07-26 17:21:52 -0300

    feat: Add relay init

--- src/drivers/relay_ctrl/relay_ctrl.c
+++ src/drivers/relay_ctrl/relay_ctrl.c
@@ -4,8 +4,6 @@
 
 #include "relay_ctrl.h"
 
-#include <zephyr/drivers/gpio.h>
-
 static const struct gpio_dt_spec enable_pin_spec =
 	GPIO_DT_SPEC_GET(DT_NODELABEL(relay), enable_gpios);
 
@@ -17,7 +15,38 @@ static const struct gpio_dt_spec data_pin_spec = GPIO_DT_SPEC_GET(DT_NODELABEL(r
 // static const struct gpio_dt_spec reset_pin_spec =
 // 	GPIO_DT_SPEC_GET(DT_NODELABEL(relay), reset_gpios);
 
-int relay_ctrl_init()
+int relay_ctrl_init(void)
 {
+	int err;
+
+	if (gpio_is_ready_dt(&enable_pin_spec) != 0) {
+		return -ENODEV;
+	}
+	if (gpio_is_ready_dt(&clock_pin_spec) != 0) {
+		return -ENODEV;
+	}
+	if (gpio_is_ready_dt(&data_pin_spec) != 0) {
+		return -ENODEV;
+	}
+
+	err = gpio_pin_configure_dt(&enable_pin_spec, GPIO_OUTPUT_HIGH);
+	if (err != 0) {
+		return err;
+	}
+	err = gpio_pin_configure_dt(&clock_pin_spec, GPIO_OUTPUT_HIGH);
+	if (err != 0) {
+		return err;
+	}
+	err = gpio_pin_configure_dt(&data_pin_spec, GPIO_OUTPUT_HIGH);
+	if (err != 0) {
+		return err;
+	}
+
+	return 0;
+}
+
+int relay_ctrl_set(uint32_t toggle)
+{
+
 	return 0;
 }
--- src/drivers/relay_ctrl/relay_ctrl.h
+++ src/drivers/relay_ctrl/relay_ctrl.h
@@ -5,6 +5,11 @@
 #ifndef RELAY_CTRL_H
 #define RELAY_CTRL_H
 
+#include <stdint.h>
+#include <zephyr/drivers/gpio.h>
+
 int relay_ctrl_init();
+
+int relay_ctrl_set(uint32_t toggle);
 
 #endif // RELAY_CTRL_H
